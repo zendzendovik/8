e3ae1796dbac195161b2f140749a6f34c0793977
zendzendovik <zendzendovik@gmail.com>
2025-09-21 02:15:10 +0200

    Add files via upload

---
+++ newElements.js
@@ -0,0 +1,11 @@
+const set = new Set([1, 2]);
+console.log(set.values());
+
+// adding new elements
+set.add(3);
+console.log(set.values());
+
+// adding duplicate elements
+// does not add to Set
+set.add(1);
+console.log(set.values());
